1321a1e250c751b8f4295a2d63bc143223703b3f
Allegro5454 <szatkowskidawid5@gmail.com>
2026-07-16 10:43:44 +0200

    Final version of Simple system monitor

---
+++ 01. System Monitor/README.md
@@ -0,0 +1,9 @@
+# SYS_MON = System Monitor
+
+# INFO
+Script that logs information in json format.
+Information saved for different devices:
+- `Disks = Usage`
+- `Network Interface = IP address`
+- `Service = State`
+
--- 01. System Monitor/SYS_MON.py
+++ 01. System Monitor/SYS_MON.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import subprocess
 import psutil
+import json
 class SystemResource:
     def __init__(self, name, status):
         self.name = name
@@ -8,7 +9,7 @@ class SystemResource:
     def check_health(self):
         pass
     def get_metrics(self):
-        pass
+        pass    
 class ServiceManager(SystemResource):
     def start(self):
         pass
@@ -22,6 +23,11 @@ class ServiceManager(SystemResource):
             return F'[Service] {self.name}: Running '
         else:
             return f'[Service] {self.name}: Down '
+    def to_dict(self):
+        return {"type":"ServiceManager",
+                "name":self.name,
+                "status":self.is_active()
+                }
 class StorageDrive(SystemResource):
     def __init__(self, name, status, mount_point):
         super().__init__(name, status)
@@ -44,6 +50,13 @@ class StorageDrive(SystemResource):
     def check_health(self):
         disk_info = self.warn_if_full()
         return F'[DRIVE] {self.name} {self.mount_point} {disk_info}' 
+    def to_dict(self):
+        return {"type":"StorageDrive",
+                "name":self.name,
+                "status":self.status,
+                "usage":self.calculate_used_percentage()
+                }
+    
 class NetworkInterface(SystemResource):
     def __init__(self, name, status):
         super().__init__(name, status)
@@ -58,29 +71,34 @@ class NetworkInterface(SystemResource):
     def check_health(self):
         addr = self.get_ip_address()
         return F'[NETWORK] {self.name} IP:{addr}'             
+    def to_dict(self):
+        return {"type":"NetworkInterface",
+                "name":self.name,
+                "address":self.get_ip_address(),
+                }
+    
 class AuditorEngine:
     def __init__(self):
         self.components = [ ]
     def add_resource(self, resource):
-        self.components.append(resource)
+        self.components.append( resource )
     def run_audit(self):
+        log = [ ]
         for item in self.components:
             print (item.check_health())
-
-my_drive = StorageDrive("root","Active", "/")
-my_serv = ServiceManager("firewalld", "Active")
-my_drive2 = StorageDrive("root", "Active", "/boot/efi")
-my_serv2 = ServiceManager("update", "Active" )
-my_drive3 = StorageDrive("root", "Active", "/boost/efi")
-my_interf = NetworkInterface("lo", "Net")
-my_interf2 = NetworkInterface("lo1", "Net")
-
+            log.append(item.to_dict())
+        with open('audit_log.json', 'w') as log_file:
+            json.dump(log, log_file, indent=4)
 engine = AuditorEngine()
-engine.add_resource(my_drive)
-engine.add_resource(my_serv)
-engine.add_resource(my_drive2)
-engine.add_resource(my_serv2)
-engine.add_resource(my_drive3)
-engine.add_resource(my_interf)
-engine.add_resource(my_interf2)
+
+with open('config.json', 'r') as file:
+    config_data = json.load(file)
+    for elem in config_data:
+        if elem["type"] == "ServiceManager":
+            engine.add_resource( ServiceManager(elem["name"], elem["status"]) )
+        elif elem["type"] == "StorageDrive":
+            engine.add_resource( StorageDrive( elem["name"], elem["status"], elem["mount_point"]))
+        elif elem["type"] == "NetworkInterface":
+            engine.add_resource( NetworkInterface(elem["name"], elem["status"]))
+ 
 engine.run_audit()
